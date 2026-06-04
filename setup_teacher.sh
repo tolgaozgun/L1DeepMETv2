d6a758e040200d89cb6b9a76549bd6f4aee5af29
Tolga Ozgun <tolgaozgunn@gmail.com>
2026-06-04 14:38:05 -0700

    feat: Add teacher model

--- setup_teacher.sh
+++ setup_teacher.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Cloning DeepMETv2 (Teacher)..."
-git clone https://github.com/DeepMETv2/graph-met.git DeepMETv2
-cd DeepMETv2
+git clone https://github.com/DeepMETv2/graph-met.git teacher-model
+cd teacher-model
 git checkout 72dd1015720edba06f8e3dd36a0de6b48c23f497
 cd ..
 
